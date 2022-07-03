90bb27811fd631afddfcbb4d446e22731fa939fd
podgeNoob <conorrice@live.co.uk>
2022-07-03 23:08:42 +0100

    initial commit to updateRepo

---
+++ updatesPractice/src/main/java/com/example/updatesPractice/UpdatesPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.updatesPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpdatesPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpdatesPracticeApplication.class, args);
+	}
+
+}
